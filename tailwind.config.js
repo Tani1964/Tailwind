c5ee190a89fc9a55a274899034336549dc73443a
TANITOLUWA IFEGBESAN <101012325+Tani1964@users.noreply.github.com>
2023-04-19 07:24:49 +0000

    installed tailwind dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html'
+  ],
   content: [],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
